5506b62234796d1a797038d6dc9bfce733aeb88f
Emmanuel Mireku <34449042+emmanuel-mireku@users.noreply.github.com>
2021-12-05 13:43:17 -0500

    Initial solution
    I know there is another way of using charCodeAt string method of solving this issue.

---
+++ solution.js
@@ -0,0 +1,64 @@
+// first solution is to store the morse code characters in a map
+// and encode the morse code with the map reference.
+
+let morseCodeCharsAndVals = [
+  [".-", "A"],
+  ["-...", "B"],
+  ["-.-.", "C"],
+  ["-..", "D"],
+  [".", "E"],
+  ["..-.", "F"],
+  ["--.", "G"],
+  ["....", "H"],
+  ["..", "I"],
+  [".---", "J"],
+  ["-.-", "K"],
+  [".-..", "L"],
+  ["--", "M"],
+  ["-.", "N"],
+  ["---", "O"],
+  [".--.", "P"],
+  ["--.-", "R"],
+  [".-.", "S"],
+  ["...", "T"],
+  ["..-", "U"],
+  ["...-", "V"],
+  [".--", "W"],
+  ["-..-", "X"],
+  ["-.--", "Y"],
+  ["--..", "Z"],
+  ["-----", "0"],
+  [".----", "1"],
+  ["..---", "2"],
+  ["...--", "3"],
+  ["....-", "4"],
+  [".....", "5"],
+  ["-....", "6"],
+  ["--...", "7"],
+  ["---..", "8"],
+  ["----.", "9"],
+];
+
+let morseCodeMap = new Map(morseCodeCharsAndVals);
+
+let encodeMorseCode = (code) => {
+  let result = [];
+  let input = code.toString().split("  ");
+  /* let input = stringWithoutDoubleSpaces.toString().split(" ") */
+  console.log(input, "test");
+  for (let i = 0; i < input.length; i++) {
+    let subResult = [];
+    let subInput = input[i].split(" ");
+    for (let j = 0; j < subInput.length; j++) {
+      console.log(morseCodeMap.get(subInput[j]));
+      if (morseCodeMap.has(subInput[j]))
+        subResult.push(morseCodeMap.get(subInput[j]));
+    }
+    result.push(subResult.join(""));
+  }
+  return result.join(" ");
+};
+
+let input = ". -- -- .- -. ..- . .-..  -- .. --.- . -.- ..-";
+console.log(encodeMorseCode(input));
+// "EMMANUEL MIREKU"
